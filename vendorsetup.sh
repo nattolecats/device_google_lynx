cd9698bfdc0df9a8f6f587ed3870f5163b9ed1fc
lahaina <nattolecats@gmail.com>
2023-05-18 20:39:46 +0900

    lynx: vendorsetup: Create symlinks for building dtb and dtbo make[1]: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/out/target/product/lynx/obj/DTBO_OBJ' から出ます make: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google' から出ます make: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google'　に入ります make[1]: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/out/target/product/lynx/obj/DTBO_OBJ' に入ります /home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google/scripts/Makefile.build:45: /home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google/arch/arm64/boot/dts/google/devices/lynx/google-base/Makefile: そのようなファイルやディレクトリはありません make[2]: *** ターゲット '/home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google/arch/arm64/boot/dts/google/devices/lynx/google-base/Makefile' を make するルールがありません.  中止. make[1]: *** [/home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google/Makefile:1436: google/devices/lynx/google-base/gs201-a0.dtb] エラー 2 make[1]: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/out/target/product/lynx/obj/DTBO_OBJ' から出ます make: *** [Makefile:185: __sub-make] エラー 2 make: ディレクトリ '/home/lahaina/evo/tiramisu-pixel/kernel/google/gs201/private/gs-google' から出ます

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,10 @@ croot
 rm -rf vendor/gms/product/packages/privileged_apps/DeviceIntelligenceNetworkPrebuilt
 rm -rf vendor/gms/product/packages/privileged_apps/DevicePersonalizationPrebuiltPixel2020
 
+# Create DTS symlink if missing
+SYMLINK_TARGET=kernel/google/gs201/private/gs-google/arch/arm64/boot/dts/google/devices
+mkdir -p $SYMLINK_TARGET && ln -sf ../../../../../../../devices/google/lynx/dts/ $SYMLINK_TARGET/lynx
+
 cd - > /dev/null
 
 return
